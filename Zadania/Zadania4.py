53c3def04a573087aa4de70f2a81865f6857c719
SynProboszcza <SynProboszcza>
2021-03-21 21:03:30 +0100

    zadanie 4

--- Zadania/Zadania4.py
+++ Zadania/Zadania4.py
@@ -19,6 +19,7 @@ plik.close()
 #Zad3
 # Wykorzystując komendę with zapisz kilka linijek tekstu
 # do pliku a następnie wyświetl je na ekranie.
+"""
 import sys
 
 dane = input("Podaj mi trochę tekstu, wpisze go do pliku:")
@@ -30,7 +31,46 @@ plik.close()
 with open("kilka_linijek.txt", "r", encoding="utf8") as plik:
     print(plik.read())
 plik.close()
+"""
 
+#Zad4
+# Stwórz klasę NaZakupy, która będzie przechowywać atrybuty: ...
+"""
+class NaZakupy:
+    def __init__(self,nazwa_produktu,ilosc,jednostka_miary,cena_jed):
+        self.nazwa_produktu = nazwa_produktu
+        self.ilosc = float(ilosc)
+        self.jednostka_miary = jednostka_miary
+        self.cena_jed = float(cena_jed)
+    def wyswietl_produkt(self):
+        print(self.nazwa_produktu)
+        print(self.ilosc)
+        print(self.jednostka_miary)
+        print(self.cena_jed)
+    def ile_produktu(self):
+        print(str(self.ilosc) + str(self.jednostka_miary))
+    def ile_kosztuje(self):
+        cena = self.ilosc * self.cena_jed
+        print(str(cena))
+
+
+
+
+
+zak = NaZakupy("mleko",10,"litr",12)
+
+# print(zak.nazwa_produktu)
+# print(zak.ilosc)
+# print(zak.jednostka_miary)
+# print(zak.cena_jed)
+# niepotrzebne bo wyswietl_produkt()
+
+zak.wyswietl_produkt()
+print()
+zak.ile_produktu()
+print()
+zak.ile_kosztuje()
+"""
 
 
 
