93a300eda0adf24fda7bca4fe747bb8d792d4e17
SynProboszcza <SynProboszcza>
2021-05-23 22:43:59 +0200

    matplotlib zad2

--- Zadania/matplotlib1 zadania.py
+++ Zadania/matplotlib1 zadania.py
@@ -6,11 +6,19 @@ import matplotlib.pyplot as plt
 # plt.legend()
 # plt.xlabel("x")
 # plt.ylabel("f(x)")
+# plt.ylim(ymin=0.02)
 # plt.show()
 
 #Zad2
 
-
+# tab = [1/x for x in range(20,41)]
+# plt.plot(tab, "b--", tab, "bo")
+# plt.title("Wykres funckji f(x) dla x[20,40]")
+# plt.xlabel("x")
+# plt.ylabel("1/x")
+# plt.margins(0)
+# plt.ylim(ymin=0.02)
+# plt.show()
 
 
 
