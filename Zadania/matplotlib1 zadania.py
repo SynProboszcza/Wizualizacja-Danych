58f4df0515adad458c9dbf03cc525dcfdb564b51
SynProboszcza <SynProboszcza>
2021-05-23 22:31:31 +0200

    matplotlib zad1

---
+++ Zadania/matplotlib1 zadania.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+#Zad1
+# tab = [1/x for x in range(20,41)]
+# plt.plot(tab, label="1/x")
+# plt.legend()
+# plt.xlabel("x")
+# plt.ylabel("f(x)")
+# plt.show()
+
+#Zad2
+
+
+
+
+
+
+
+
+
+
+
