1e717b1867b0dc4421df6ad604fd3f5fcd71026a
SynProboszcza <SynProboszcza>
2021-05-24 14:07:50 +0200

    matplotlib2 zad1 i 2

---
+++ Zadania/matplotlib2.py
@@ -0,0 +1,60 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Zad1
+
+# fig = plt.figure()
+# ax = fig.gca(projection='3d')
+#
+# z = np.linspace(0, 2*np.pi, 100)
+# x = np.sin(z)
+# y = 2*np.cos(z)
+#
+# ax.plot(x, y, z)
+# plt.show()
+
+#Zad2
+
+# np.random.seed(18912837)
+#
+# def randrange(n, vmin, vmax):
+#     return (vmax - vmin) * np.random.rand(n) + vmin
+#
+# fig = plt.figure()
+# ax = fig.add_subplot(projection='3d')
+# n = 100
+#
+# for c, m, zlow, zhigh in [('tan', '.', 1, 2),('indigo', '1', 3, 4),('mediumturquoise', '*', 5, 6),('aquamarine', '*', 7, 8),('lime', '*', 9, 10)]:
+#     xs = randrange(n, 23, 32)
+#     ys = randrange(n, 0, 100)
+#     zs = randrange(n, zlow, zhigh)
+#     ax.scatter(xs, ys, zs, c=c, marker=m)
+#
+# ax.set_xlabel('x')
+# ax.set_ylabel('y')
+# ax.set_zlabel('z')
+#
+# plt.show()
+
+#Zad3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
